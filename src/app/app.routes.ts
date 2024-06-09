b86e420cc62f86432fea0a25fead98c76b14c398
Chris Ebert <cebert@gmail.com>
2024-06-09 13:56:03 -0400

    fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 export const routes: Routes = [
    { path: '', redirectTo: '/home', pathMatch: 'full' },

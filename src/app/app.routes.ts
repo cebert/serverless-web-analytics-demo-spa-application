1a178343dd162853e75b440c3e677e2644aad9b4
Chris Ebert <cebert@gmail.com>
2024-06-10 21:09:51 -0400

    fixed button display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from "@angular/router";
-import { AboutComponent } from "./about/about.component";
-import { ButtonEventTrackingComponent } from "./button-event-tracking/button-event-tracking.component";
-import { HomeComponent } from "./home/home.component";
+import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ButtonEventTrackingComponent } from './button-event-tracking/button-event-tracking.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
    { path: '', component: HomeComponent },

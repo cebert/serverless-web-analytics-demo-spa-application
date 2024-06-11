3b80318ae1434b8db354831717ebb7726c5e6a0b
Chris Ebert <cebert@gmail.com>
2024-06-10 21:07:52 -0400

    Fixed URL redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ButtonEventTrackingComponent } from './button-event-tracking/button-event-tracking.component';
-import { HomeComponent } from './home/home.component';
+import { Routes } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { ButtonEventTrackingComponent } from "./button-event-tracking/button-event-tracking.component";
+import { HomeComponent } from "./home/home.component";
 
 export const routes: Routes = [
    { path: '', component: HomeComponent },
    { path: 'home', component: HomeComponent },
    { path: 'event-tracking', component: ButtonEventTrackingComponent },
    { path: 'about', component: AboutComponent },
-   { path: '**', redirectTo: '/home' },
+   { path: '**', redirectTo: '' },
 ];
